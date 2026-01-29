d7b9062ff735efb9f24ba474401fdbc607442034
lichbich <tronglich.a2@gmail.com>
2026-01-29 21:01:28 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // serverActions: true, // Enabled by default in Next 14+
+  }
 };
 
 export default nextConfig;
